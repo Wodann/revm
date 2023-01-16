f3f8866fbf48ca70acc684d557eaad8c1b1b3830
Wodann <Wodann@users.noreply.github.com>
2023-01-16 00:51:31 -0600

    feat: implement State trait for Arc<StateRef>

---
+++ crates/revm/src/blockchain.rs
@@ -0,0 +1,42 @@
+use auto_impl::auto_impl;
+use revm_interpreter::{B256, U256};
+
+#[auto_impl(& mut, Box)]
+pub trait BlockHash {
+    type Error;
+
+    /// Get block hash by block number
+    fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error>;
+}
+
+#[auto_impl(&, Box, Arc)]
+pub trait BlockHashRef {
+    type Error;
+
+    /// Get block hash by block number
+    fn block_hash(&self, number: U256) -> Result<B256, Self::Error>;
+}
+
+impl<T> BlockHash for &T
+where
+    T: BlockHashRef,
+{
+    type Error = <T as BlockHashRef>::Error;
+
+    fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error> {
+        BlockHashRef::block_hash(*self, number)
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T> BlockHash for std::sync::Arc<T>
+where
+    T: BlockHashRef,
+{
+    type Error = <T as BlockHashRef>::Error;
+
+    fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error> {
+        use std::ops::Deref;
+        self.deref().block_hash(number)
+    }
+}
